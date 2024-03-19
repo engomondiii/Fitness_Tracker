5144a28018e303a063c490c24546ecbcdff49422
engomondiii <engomondiii@gmail.com>
2024-03-19 16:42:41 +0300

    added these files

---
+++ public/mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+$mail_host = "smtp.gmail.com";
+$mail_port = "587";
+$mail_sender_email = "info.dropke@gmail.com"; // Your Gmail address
+$mail_sender_password = "Stretch@123!@#"; // Your Gmail password
+$mail_sender_name = "Diabetes Remission and Obesity Prevention"; // Sender's name
+?>
